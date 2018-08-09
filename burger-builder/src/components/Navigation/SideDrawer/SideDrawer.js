c6270843a4d585c222ba4f90a9c9426e64ce9d5a
Christian Rodriguez <rodchris3090452@gmail.com>
2018-08-08 23:34:14 -0400

    added side drawer and adjusted responsiveness design

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
-import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 
-const toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <div>MENU</div>
+import classes from './SideDrawer.css';
+
+const sideDrawer = (props) => {
+  return (
+    <div className={classes.SideDrawer}>
         <div className={classes.Logo}>
             <Logo />
-        </div>
-        <nav className={classes.DesktopOnly}>
+        </div>        
+        <nav>
             <NavigationItems />
         </nav>
-    </header>
-);
+    </div>
+  )
+}
 
-export default toolbar;
+export default sideDrawer;
