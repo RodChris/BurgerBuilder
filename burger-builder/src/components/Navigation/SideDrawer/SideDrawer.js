4a4da1e5042ed73b9e44e531b7cea435d38365bb
Christian Rodriguez <rodchris3090452@gmail.com>
2018-08-10 00:10:59 -0400

    added backdrop to side drawer and improved toolbar design

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,20 +2,31 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxiliary from '../../../hoc/Auxiliary';
 
 import classes from './SideDrawer.css';
 
 const sideDrawer = (props) => {
-  return (
-    <div className={classes.SideDrawer}>
-        <div className={classes.Logo}>
-            <Logo />
-        </div>        
-        <nav>
-            <NavigationItems />
-        </nav>
-    </div>
-  )
+
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open]
+    }
+
+    return (
+        <Auxiliary>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>        
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+            </Auxiliary>
+    )
 }
 
 export default sideDrawer;
