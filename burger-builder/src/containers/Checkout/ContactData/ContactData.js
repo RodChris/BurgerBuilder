608472a5fa835c072ca1479ec0cdfb26b65948fb
Christian Rodriguez <rodchris3090452@gmail.com>
2018-09-10 23:04:52 -0400

    Added validation to contact data form

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -17,7 +17,12 @@ import classes from './ContactData.css'
                     type: 'text',
                     placeHolder: 'Enter your name'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },        
             street: {
                 elementType: 'input',
@@ -25,7 +30,12 @@ import classes from './ContactData.css'
                     type: 'text',
                     placeHolder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },        
             zipCode: {
                 elementType: 'input',
@@ -33,7 +43,14 @@ import classes from './ContactData.css'
                     type: 'text',
                     placeHolder: 'Zip Code'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false,
+                touched: false
             },        
             country: {
                 elementType: 'input',
@@ -41,7 +58,12 @@ import classes from './ContactData.css'
                     type: 'text',
                     placeHolder: 'Country'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },        
             phoneNumber: {
                 elementType: 'input',
@@ -49,7 +71,12 @@ import classes from './ContactData.css'
                     type: 'text',
                     placeHolder: 'Phone Number'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -90,6 +117,24 @@ import classes from './ContactData.css'
             })
         }
 
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if (rules.required) {
+            isValid= value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        return isValid;
+    }
+
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {
             ...this.state.orderForm
@@ -98,7 +143,10 @@ import classes from './ContactData.css'
             ...updatedOrderForm[inputIdentifier]
         };
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+        updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
+        console.log(updatedFormElement);
         this.setState({orderForm: updatedOrderForm});
     }
 
@@ -118,6 +166,9 @@ import classes from './ContactData.css'
                         elementType={formElement.config.elementType} 
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value} 
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                 ))}                 
                 <Button btnType='Success' clicked={this.orderHandler}>Order</Button>                
