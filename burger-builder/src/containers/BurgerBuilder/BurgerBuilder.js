2d796c2992900b57359e50570f81298012080cdd
Christian Rodriguez <rodchris3090452@gmail.com>
2018-12-26 00:21:53 -0500

    Updated reducer for ingredients

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,13 +27,13 @@ class BurgerBuilder extends Component {
   };
 
   componentDidMount () {
-      axios.get('https://react-my-burger-e06fd.firebaseio.com/ingredients.json')
-            .then(response => {
-            this.setState({ingredients: response.data});
-        })
-        .catch(error => {
-            this.setState({error: true})
-        });
+    //   axios.get('https://react-my-burger-e06fd.firebaseio.com/ingredients.json')
+    //         .then(response => {
+    //         this.setState({ingredients: response.data});
+    //     })
+    //     .catch(error => {
+    //         this.setState({error: true})
+    //     });
   }
 
   updatePurchaseState (ingredients) {
